2c473edc47f86c79c5c3a66b74f51160068083dd
Drilon <drilonsulejmani305@gmail.com>
2021-11-21 01:12:05 +0100

    deleted component folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './components/index.css'
+import './index.css'
 import styled from 'styled-components'
-import Navbar from './components/Navbar';
+import Navbar from './Navbar';
 
 
 const App = () => {
